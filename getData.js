2d463501f4dd8de3a3f2cc9e5ce394b51c25623a
AirplaneGoBrr <71111395+AirplanegoBrr@users.noreply.github.com>
2022-03-24 10:56:23 -0700

    crap

--- getData.js
+++
@@ -1,2 +0,0 @@
-const news = require('./news');
-
--- index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const news = require('./news');
-
-//every minute, check if news.json exists
-
-news.getData();
-setInterval(() => {
-    news.getData();
-}, 60000);
