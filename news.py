475b8b9703e1c1a92a9cdbbece0a7d003e022a32
AirplaneGoBrr <71111395+AirplanegoBrr@users.noreply.github.com>
2022-03-24 11:07:24 -0700

    Added try to news
    (Also sorry for 6 commits ahead. I pushed the wrong code to the repo)

--- news.py
+++ news.py
@@ -23,104 +23,76 @@ def url() -> str:
     return f"https://push.api.bbci.co.uk/batch?t=%2Fdata%2Fbbc-morph-lx-commentary-data-paged%2FassetUri%2F%252Fnews%252Flive%252Fworld-europe-{latest_url}%2FisUk%2Ffalse%2Flimit%2F20%2FnitroKey%2Flx-nitro%2FpageNumber%2F1%2FserviceName%2Fnews%2Fversion%2F1.5.6?timeout=5"
 
 async def get_data() -> Union[dict, None]:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url()) as resp:
-            console.log("Fetching data...")
-            data = await resp.json()
-            global latest_data
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url()) as resp:
+                console.log("Fetching data...")
+                data = await resp.json()
+                global latest_data
 
-            latest = data["payload"][0]["body"]["results"][0]
-            latest_id = latest_data["news_id"]
+                latest = data["payload"][0]["body"]["results"][0]
+                latest_id = latest_data["news_id"]
 
-            # This is just in case they post a new update while im still working on adding a new datatype
-            # for i in data["payload"][0]["body"]["results"]:
-            #     if i["assetId"] == "621a7884980bea49f4b7a320":
-            #         latest = i
+                # This is just in case they post a new update while im still working on adding a new datatype
+                # for i in data["payload"][0]["body"]["results"]:
+                #     if i["assetId"] == "621a7884980bea49f4b7a320":
+                #         latest = i
 
-            if latest["assetId"] == latest_id:
-                console.log("No new data.")
-                return None
+                if latest["assetId"] == latest_id:
+                    console.log("No new data.")
+                    return None
 
-            latest_data["news_id"] = latest["assetId"]
+                latest_data["news_id"] = latest["assetId"]
 
-            title = latest["title"]
-            content = "" # Gotten later
-            image_url = None
-            is_breaking = latest["options"]["isBreakingNews"]
-            post_locator = latest["locator"]
-            updated = datetime.strptime(latest["lastUpdated"].rstrip(":"), "%Y-%m-%dT%H:%M:%S%z")
+                title = latest["title"]
+                content = "" # Gotten later
+                image_url = None
+                is_breaking = latest["options"]["isBreakingNews"]
+                post_locator = latest["locator"]
+                updated = datetime.strptime(latest["lastUpdated"].rstrip(":"), "%Y-%m-%dT%H:%M:%S%z")
 
-            # Image
-            try:
-                for image_key in latest["media"]["images"]["body"]:
-                    image_url = latest["media"]["images"]["body"][image_key]["href"]
-                    break
-            except: image_url = None
-
-            # Content
-            # TODO: Remake this to be more readable and less code re-write
-            # It seems that every data type supports every other data type
-            # -------
-            # Right now it loops through every known data type and adds what is needed.
-            for item in latest["body"]:
-
+                # Image
                 try:
-                    if item["name"] == "paragraph":
-                        if len(item["children"]) == 1:
-                            if item["children"][0]["name"] == "text":
-                                content += item["children"][0]["text"].replace("\n", " ")
+                    for image_key in latest["media"]["images"]["body"]:
+                        image_url = latest["media"]["images"]["body"][image_key]["href"]
+                        break
+                except: image_url = None
 
-                            elif item["children"][0]["name"] == "link":
-                                    text = child["children"][0]["children"][0]["text"]
-                                    text_url = child["children"][2]["attributes"][1]["value"]
+                # Content
+                # TODO: Remake this to be more readable and less code re-write
+                # It seems that every data type supports every other data type
+                # -------
+                # Right now it loops through every known data type and adds what is needed.
+                for item in latest["body"]:
 
-                                    if text_url.startswith("https://www.bbc.co.uk/news/live/world-europe-") or text_url.startswith("https://www.bbc.com/news/live/world-europe-"):
-                                        latest_data["news_url"] = text_url.split("-")[-1]
-                                        console.log(f"Changed news url to {latest_data['news_url']}")
-                                        return None
+                    try:
+                        if item["name"] == "paragraph":
+                            if len(item["children"]) == 1:
+                                if item["children"][0]["name"] == "text":
+                                    content += item["children"][0]["text"].replace("\n", " ")
 
-                                    content += f"[{text}]({text_url})"
+                                elif item["children"][0]["name"] == "link":
+                                        text = child["children"][0]["children"][0]["text"]
+                                        text_url = child["children"][2]["attributes"][1]["value"]
 
-                            elif item["children"][0]["name"] == "bold":
-                                    content += "**" + item["children"][0]["children"][0]["text"].strip() + "**"
+                                        if text_url.startswith("https://www.bbc.co.uk/news/live/world-europe-") or text_url.startswith("https://www.bbc.com/news/live/world-europe-"):
+                                            latest_data["news_url"] = text_url.split("-")[-1]
+                                            console.log(f"Changed news url to {latest_data['news_url']}")
+                                            return None
 
-                        else:
-                            for child in item["children"]:
-                                if child["name"] == "text":
-                                    content += child["text"].replace("\n\n", " ")
+                                        content += f"[{text}]({text_url})"
 
-                                elif child["name"] == "link":
-                                    text = child["children"][0]["children"][0]["text"]
-                                    text_url = child["children"][2]["attributes"][1]["value"]
+                                elif item["children"][0]["name"] == "bold":
+                                        content += "**" + item["children"][0]["children"][0]["text"].strip() + "**"
 
-                                    if text_url.startswith("https://www.bbc.co.uk/news/live/world-europe-") or text_url.startswith("https://www.bbc.com/news/live/world-europe-"):
-                                        latest_data["news_url"] = text_url.split("-")[-1]
-                                        console.log(f"Changed news url to {latest_data['news_url']}")
-                                        return None
-
-                                    content += f"[{text}]({text_url}) "
-
-                                elif child["name"] == "bold":
-                                        content += "**" + child["children"][0]["text"].strip() + "** "
-                            
-                        content += "\n\n"
-
-                    elif item["name"] == "list":
-                        for child in item["children"]:
-                            if len(child["children"]) == 1:
-                                if child["name"] == "listItem":
-                                    content += " · " + child["children"][0]["text"].strip()
-                            
                             else:
-                                content += " · "
-                                for sub_child in child["children"]:
+                                for child in item["children"]:
+                                    if child["name"] == "text":
+                                        content += child["text"].replace("\n\n", " ")
 
-                                    if sub_child["name"] == "text":
-                                        content += sub_child["text"].strip() + " "
-
-                                    elif sub_child["name"] == "link":
-                                        text = sub_child["children"][0]["children"][0]["text"]
-                                        text_url = sub_child["children"][2]["attributes"][1]["value"]
+                                    elif child["name"] == "link":
+                                        text = child["children"][0]["children"][0]["text"]
+                                        text_url = child["children"][2]["attributes"][1]["value"]
 
                                         if text_url.startswith("https://www.bbc.co.uk/news/live/world-europe-") or text_url.startswith("https://www.bbc.com/news/live/world-europe-"):
                                             latest_data["news_url"] = text_url.split("-")[-1]
@@ -129,56 +101,93 @@ async def get_data() -> Union[dict, None]:
 
                                         content += f"[{text}]({text_url}) "
 
-                                    elif sub_child["name"] == "bold":
-                                        content += "**" + sub_child["children"][0]["text"].strip() + "** "
+                                    elif child["name"] == "bold":
+                                            content += "**" + child["children"][0]["text"].strip() + "** "
 
                             content += "\n\n"
 
-                    elif item["name"] == "link":
-                        text = item["children"][0]["children"][0]["text"]
-                        text_url = item["children"][2]["attributes"][1]["value"]
+                        elif item["name"] == "list":
+                            for child in item["children"]:
+                                if len(child["children"]) == 1:
+                                    if child["name"] == "listItem":
+                                        content += " · " + child["children"][0]["text"].strip()
 
-                        if text_url.startswith("https://www.bbc.co.uk/news/live/world-europe-") or text_url.startswith("https://www.bbc.com/news/live/world-europe-"):
-                            latest_data["news_url"] = text_url.split("-")[-1]
-                            console.log(f"Changed news url to {latest_data['news_url']}")
-                            return None
+                                else:
+                                    content += " · "
+                                    for sub_child in child["children"]:
 
-                        content += f"[{text}]({text_url})\n\n"
+                                        if sub_child["name"] == "text":
+                                            content += sub_child["text"].strip() + " "
 
-                    elif item["name"] == "video":
-                        content += "*There is a video, but the bot cannot display it. Please click on the link above to view it.*\n\n"
+                                        elif sub_child["name"] == "link":
+                                            text = sub_child["children"][0]["children"][0]["text"]
+                                            text_url = sub_child["children"][2]["attributes"][1]["value"]
 
-                    elif item["name"] == "quote":
-                        if item["children"][0]["name"] == "quoteText":
-                            content += "\"" + item["children"][0]["children"][0]["text"].replace("\"", "") + "\"\n\n"
+                                            if text_url.startswith("https://www.bbc.co.uk/news/live/world-europe-") or text_url.startswith("https://www.bbc.com/news/live/world-europe-"):
+                                                latest_data["news_url"] = text_url.split("-")[-1]
+                                                console.log(f"Changed news url to {latest_data['news_url']}")
+                                                return None
 
-                    elif item["name"] == "embed":
-                        try:
-                            if item["children"][0]["children"][0]["text"] == "twitter":
-                                t_url = item["children"][1]["children"][0]["text"]
-                                content += f"[Twitter]({t_url})\n\n"
-                        except: pass
+                                            content += f"[{text}]({text_url}) "
 
-                except:
-                    pass
+                                        elif sub_child["name"] == "bold":
+                                            content += "**" + sub_child["children"][0]["text"].strip() + "** "
 
-                if len(content) > 4096:
-                    content = content[:4093] + "..."
-                    console.warn("Content is over 4096 chars long. Truncated.")
-                    break
+                                content += "\n\n"
 
-            
-            content = content.strip()
+                        elif item["name"] == "link":
+                            text = item["children"][0]["children"][0]["text"]
+                            text_url = item["children"][2]["attributes"][1]["value"]
 
-            with open("latest.json", "w+") as f:
-                json.dump(latest_data, f, indent=4)
+                            if text_url.startswith("https://www.bbc.co.uk/news/live/world-europe-") or text_url.startswith("https://www.bbc.com/news/live/world-europe-"):
+                                latest_data["news_url"] = text_url.split("-")[-1]
+                                console.log(f"Changed news url to {latest_data['news_url']}")
+                                return None
 
-            return {
-                "title": title,
-                "content": content,
-                "is_breaking": is_breaking,
-                "image": image_url,
-                "locator": post_locator,
-                "updated": updated,
-                "news_url": latest_data["news_url"]
-            }
+                            content += f"[{text}]({text_url})\n\n"
+
+                        elif item["name"] == "video":
+                            content += "*There is a video, but the bot cannot display it. Please click on the link above to view it.*\n\n"
+
+                        elif item["name"] == "quote":
+                            if item["children"][0]["name"] == "quoteText":
+                                content += "\"" + item["children"][0]["children"][0]["text"].replace("\"", "") + "\"\n\n"
+
+                        elif item["name"] == "embed":
+                            try:
+                                if item["children"][0]["children"][0]["text"] == "twitter":
+                                    t_url = item["children"][1]["children"][0]["text"]
+                                    content += f"[Twitter]({t_url})\n\n"
+                            except: pass
+
+                    except:
+                        pass
+
+                    if len(content) > 4096:
+                        content = content[:4093] + "..."
+                        console.warn("Content is over 4096 chars long. Truncated.")
+                        break
+
+                    
+                content = content.strip()
+
+                with open("latest.json", "w+") as f:
+                    json.dump(latest_data, f, indent=4)
+
+                # Error checking
+                if title == "":
+                    title = "No title"
+                if content == "":
+                    content = "No content"
+
+                return {
+                    "title": title,
+                    "content": content,
+                    "is_breaking": is_breaking,
+                    "image": image_url,
+                    "locator": post_locator,
+                    "updated": updated,
+                }
+    except:
+        console.error("Failed to parse latest data")
+        return None
